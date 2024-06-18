1373386e8690212e2bc2084b40941f2b903538c8
ghofrane.yakoubi <ghofrane.yakoubi@bloomkeys.com>
2024-06-18 22:53:56 +0100

    Initialize project and create APIs to get all movies and get a movie by ImdbId

---
+++ src/main/java/dev/stormy/movies/Review.java
@@ -0,0 +1,17 @@
+package dev.stormy.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private Object id;
+    private String body;
+}
